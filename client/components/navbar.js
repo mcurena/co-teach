675904e87e6eac4b560f5e7add93af8f2d0a1f34
Michelle Ureña <mcurena24@gmail.com>
2019-03-21 13:01:17 -0400

    refactor: use promise.all to load database info

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -110,9 +110,11 @@ class Navbar extends React.Component {
   };
 
   async componentDidMount() {
-    await this.props.loadStudents();
-    await this.props.loadGroups();
-    await this.props.loadObservations();
+    await Promise.all(
+      this.props.loadStudents(),
+      this.props.loadGroups(),
+      this.props.loadObservations()
+    );
     await this.createGroup();
   }
 
